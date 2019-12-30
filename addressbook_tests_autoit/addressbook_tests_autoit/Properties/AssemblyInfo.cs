ea0834d9ecd8ed5383d3c9bd7d559fbbdaa3c5c9
natasha <nkolpakova1976@mail.ru>
2019-12-30 14:41:36 +0300

    Task 18

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook_tests_autoit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbook_tests_autoit")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("94f157b6-db9b-401a-b6fe-4e69cd40e6b0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ addressbook_tests_autoit/addressbook_tests_autoit/tests/GroupRemovalTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        [Test]
+        public void TestGroupRemoval()
+        {
+            // if there is only one group we add second (due to one group can't be removed)
+            if (app.Groups.GetGroupList().Count==1)
+            {
+                GroupData newGroup = new GroupData()
+                {
+                    Name = "newGroupNN"
+                };
+                app.Groups.Add(newGroup);
+            }
+            
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            GroupData toRemove = app.Groups.GetGroupList()[0];
+            app.Groups.Remove(toRemove);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Remove(toRemove);
+            oldGroups.Sort();
+            newGroups.Sort();
+
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
